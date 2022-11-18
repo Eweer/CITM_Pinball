b5a94d020ff1d3ea8da5d038f8d99ea376ade061
Uri <oriol253@gmail.com>
2022-11-18 11:26:00 +0100

    Asset correction

--- Game/Source/Map.cpp
+++ Game/Source/Map.cpp
@@ -53,19 +53,14 @@ bool Map::Load()
     // L07 DONE 3: Create colliders
     // Later you can create a function here to load and create the colliders from the map
 
-    /*
-    PhysBody* c1 = app->physics->CreateRectangle(224 + 128, 543 + 32, 256, 64, STATIC);
-    // L07 DONE 7: Assign collider type
+    PhysBody* c1 = app->physics->CreateRectangle(224 + 128, 543 + 32, 256, 64, bodyType::STATIC);
     c1->ctype = ColliderType::PLATFORM;
 
-    PhysBody* c2 = app->physics->CreateRectangle(352 + 64, 384 + 32, 128, 64, STATIC);
-    // L07 DONE 7: Assign collider type
+    PhysBody* c2 = app->physics->CreateRectangle(352 + 64, 384 + 32, 128, 64, bodyType::STATIC);
     c2->ctype = ColliderType::PLATFORM;
 
-    PhysBody* c3 = app->physics->CreateRectangle(256, 704 + 32, 576, 64, STATIC);
-    // L07 DONE 7: Assign collider type
+    PhysBody* c3 = app->physics->CreateRectangle(256, 704 + 32, 576, 64, bodyType::STATIC);
     c3->ctype = ColliderType::PLATFORM;
-    */
 
 
     return true;
